1d59777335fdc9b01f8fa284d314a454aa0b285a
Dimitrius <shimansky.dmitry@gmail.com>
2024-05-23 20:49:45 +0300

    lesson 5: add Modal window

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -1,17 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React from "react";
 import {GlobalStyle} from "./Components/GlobalStyle";
 import {NavBar} from "./Components/NavBar";
 import {Menu} from "./Components/Menu";
-
+import {ModalItem} from "./Components/ModalItem";
 
 
 function App() {
-  return (
+
+    const [openItem, setOpenItem] = React.useState(null);
+
+    return (
       <>
           <GlobalStyle/>
           <NavBar/>
-          <Menu/>
+          <Menu setOpenItem={setOpenItem}/>
+          <ModalItem openItem={openItem} setOpenItem={setOpenItem}/>
       </>
   );
 }
