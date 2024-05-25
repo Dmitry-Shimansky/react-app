5d602959a1df300e03e8ab5c5a15fbf42e0cc789
Dimitrius <shimansky.dmitry@gmail.com>
2024-05-25 21:45:21 +0300

    lesson 7: add Refactoring

--- new-react-app/src/Components/Style/Banner.js
+++ new-react-app/src/Components/Style/Banner.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import bannerImg from "../image/banner-burger.jpeg"
+import bannerImg from "../../image/banner-burger.jpeg"
 
 export const Banner = styled.div`
     width: 100%;
