44caf049b7e9bafe0aa53b8edc4723ceadbcfd14
Dimitrius <shimansky.dmitry@gmail.com>
2024-07-28 21:51:15 +0300

    lesson 13: add useFetch

--- new-react-app/src/Components/Hooks/useTitle.js
+++ new-react-app/src/Components/Hooks/useTitle.js
@@ -3,6 +3,5 @@ import { useEffect } from 'react';
 export function useTitle(openItem) {
     useEffect(() => {
         document.title = openItem ? openItem.name : `MrDonald's`;
-        console.log(openItem);
     }, [openItem]);
 }
